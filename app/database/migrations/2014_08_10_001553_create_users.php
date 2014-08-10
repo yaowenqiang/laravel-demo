aa8cedaedc6dc18dd428df76d6176f47e1533718
yaowenqiang <yaowenqiang111@163.com>
2014-08-10 09:28:31 +0800

    add the register demo code

---
+++ app/database/migrations/2014_08_10_001553_create_users.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+        Schema::create('users',function($newTable)
+        {
+            $newTable->increments('id');
+            $newTable->string('email')->unique();
+            $newTable->string('username',100)->unique();
+            $newTable->string('password',15);
+            $newTable->string('remeber_token',100);
+            $newTable->timestamps();
+
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop('users');
+	}
+
+}
--- app/routes.php
+++ app/routes.php
@@ -68,3 +68,18 @@ Route::post('thanks', function()
     $theEmail = Input::get('email');
     return View::make('thanks')->with('theEmail',$theEmail);
 });
+Route::get('/register', function()
+{
+    return View::make('register');
+});
+
+Route::post('/register', function()
+{
+    $user = new User;
+    $user->email = Input::get('email');
+    $user->username = Input::get('username');
+    $user->password = Hash::make(Input::get('password'));
+    $user->save();
+    $theEmail = Input::get('email');
+    return View::make('thanks2')->with('theEmail',$theEmail);
+});
